7ee77be5090d8da2914c47d183947244c5025f32
Anders Sollander <asolland@mathworks.com>
2024-10-21 04:55:08 +0200

    Change routing table in test

--- apps/kv/test/kv/router_test.exs
+++ apps/kv/test/kv/router_test.exs
@@ -1,5 +1,17 @@
 defmodule KV.RouterTest do
-  use ExUnit.Case, async: true
+  use ExUnit.Case
+
+  setup_all do
+    current = Application.get_env(:kv, :routing_table)
+
+    Application.put_env(:kv, :routing_table, [
+      {?a..?m, :"foo@anders-ubu-ac"},
+      {?n..?z, :"bar@anders-ubu-ac"}
+    ])
+
+    on_exit fn -> Application.put_env(:kv, :routing_table, current) end
+  end
+
 
   @tag :distributed
   test "route requests across nodes" do
